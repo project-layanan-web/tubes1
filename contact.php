48427e2c50518a479e0e15c3b33bc70dd0193540
saepul millah <saepul.millah@student.unsil.ac.id>
2014-05-05 13:34:56 +0700

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html >
+<html>
+
+<head>
+	<title>Minimalistic Web Template</title>
+	<meta  charset="iso-8859-1" />
+	<link href="css/style.css" rel="stylesheet" type="text/css" />
+	<!--[if IE 6]>
+		<link href="css/ie6.css" rel="stylesheet" type="text/css" />
+	<![endif]-->
+	<!--[if IE 7]>
+        <link href="css/ie7.css" rel="stylesheet" type="text/css" />  
+	<![endif]-->
+</head>
+
+<body>
+
+	  <div id="background">
+			  <div id="page">
+			  
+					 <div class="header">
+						<div class="footer">
+							<div class="body">
+									<div id="sidebar">
+									    <a href="index.php"><img id="logo" src="images/logo.gif" with="154" height="74" alt="" title=""/></a>
+										
+										<ul class="navigation">
+										    <li><a  href="index.html">BERANDA</a></li>
+											<li ><a href="parsing.php">PARSING WEBSITE</a></li>
+											<li><a href="blog.php">CUACA TASIK</a></li>
+											<li class="active last"><a href="contact.php">KONTAK</a></li>
+										</ul>
+										
+										<div class="connect">
+										    <a href="http://facebook.com/saepul.pdls" class="facebook">&nbsp;</a>
+											<a href="http://twitter.com/saepul_pdl" class="twitter">&nbsp;</a>
+											
+										</div>
+										
+										<div class="footenote">
+										  <span>&copy; Copyright &copy; 2014</span>
+										  <span><a href="index.php">Saepul & Fahrul</a> all rights reserved</span>
+										</div>
+										
+									</div>
+									<div id="content">
+									          <div class="content">
+											    <ul>
+													<li>
+															<p>
+																terimakasih telah mengunjungi website kami dan mohon maaf atas semua kekurangan yang ada di website kami
+															
+															<span>saepul.millah@student.unsil.ac.id</span>
+													</li>
+												</ul>
+										     </div>
+									</div>
+							</div>
+						</div>
+					 </div>
+					 <div class="shadow">
+					 </div>
+			  </div>    
+	  </div>    
+	
+</body>
+</html>
